d26f13d3ad38856cb0a9ff919c2136dd41711e36
Paul <819418426@qq.com>
2022-03-16 23:40:29 +0800

    折线图完成

---
+++ src/views/dashboard/Card/lineChart/index.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="lineChart" ref="lineChart"></div>
+</template>
+
+<script>
+import * as echarts from "echarts";
+export default {
+  name: "lineChart",
+  mounted() {
+    let lineChart = echarts.init(this.$refs.lineChart);
+    lineChart.setOption({
+      xAxis: {
+        show: false,
+        type: "category",
+      },
+      yAxis: {
+        show: false,
+      },
+      // 系列
+      series: {
+        type: "line",
+        smooth: true,
+        data: [15, 25, 6, 45, 30, 45, 88, 65, 14],
+        // 拐点样式设置
+        itemStyle: {
+          opacity: 0,
+        },
+        // 线条颜色
+        lineStyle: {
+          color: "purple",
+        },
+        areaStyle: {
+          color: {
+            type: "linear",
+            x: 0,
+            y: 0,
+            x2: 0,
+            y2: 1,
+            colorStops: [
+              {
+                offset: 0,
+                color: "#9b59b6", // 0% 处的颜色
+              },
+              {
+                offset: 1,
+                color: "#bdc3c7", // 100% 处的颜色
+              },
+            ],
+            global: false, // 缺省为 false
+          },
+        },
+      },
+      // 布局
+      grid: {
+        left: 0,
+        top: 0,
+        right: 0,
+        bottom: 0,
+      },
+    });
+  },
+};
+</script>
+
+<style scoped>
+.lineChart {
+  width: 100%;
+  height: 100%;
+}
+</style>
