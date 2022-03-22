b97a72e55e2b639d2182bd7d0527ec0f25a3fa00
Paul <819418426@qq.com>
2022-03-22 20:39:32 +0800

    首页完成

--- src/views/dashboard/Sale/Chart/index.vue
+++ src/views/dashboard/Sale/Chart/index.vue
@@ -12,7 +12,7 @@ export default {
     return {
       eventName: "",
       // 将echarts实例绑定为响应式数据，以便可以全局调用他
-      chart: {},
+      chart: null,
     };
   },
   mounted() {
@@ -43,10 +43,6 @@ export default {
         {
           data: [],
           type: "bar",
-          // showBackground: true,
-          // backgroundStyle: {
-          //   color: "rgba(180, 180, 180, 0.2)",
-          // },
         },
       ],
       //   布局配置
@@ -57,74 +53,28 @@ export default {
         containLabel: true,
       },
     });
-    this.chart.resize()
-
+    // 展示默认数据
+    this.getChartInfo()
     // 接收sale组件的通知，获取柱状图表格数据
     this.$bus.$on("clickEvent", this.getChartInfo);
   },
   methods: {
     getChartInfo(name ="sale") {
-      name === "sale" ? (this.eventName = "销售量"): (this.eventName = "访问量");
-      // 重新更新表格chart 的配置
-      this.chart.setOption({
+      name == "sale" ? (this.eventName = "销售量"): (this.eventName = "访问量");
+      // 重新更新表格chart 的配置,由于是异步操作，故需先判断数据是否已返回
+      if (this.saleChartInfo) {
+        this.chart.setOption({
         title: { text: this.eventName + "趋势" },
         xAxis: {
           data: name == "sale"? this.saleChartInfo.orderFullYearAxis: this.saleChartInfo.userFullYearAxis,
         },
         series: {
-          data:name == "sale"? this.saleChartInfo.orderFullYear: this.saleChartInfo.userFullYear,
+          data: name == "sale"? this.saleChartInfo.orderFullYear: this.saleChartInfo.userFullYear,
         },
       });
+      }
     },
   },
-/*   watch: {
-    saleChartInfo:{
-      handler() {
-        this.$nextTick(()=>{
-          //输入函数体
-        this.chart.setOption({
-          tooltip: {
-            trigger: "axis",
-            axisPointer: {
-              type: "shadow",
-            },
-          },
-          xAxis: {
-            type: "category",
-            data: this.saleChartInfo.orderFullYearAxis,
-            axisTick: {
-              alignWithLabel: true,
-            },
-          },
-          yAxis: {
-            type: "value",
-          },
-          title: {
-            text: "销售额趋势",
-          },
-          // 系列配置
-          series: [
-            {
-              data: this.saleChartInfo.orderFullYear,
-              type: "bar",
-              // showBackground: true,
-              // backgroundStyle: {
-              //   color: "rgba(180, 180, 180, 0.2)",
-              // },
-            },
-          ],
-          //   布局配置
-          grid: {
-            // show:true,
-            left: 50,
-            bottom: 50,
-            containLabel: true,
-          },
-        })
-        })
-      },
-    }
-  }, */
   computed: {
     // 映射仓库数据
     ...mapState("homeStore", ["saleChartInfo"]),
